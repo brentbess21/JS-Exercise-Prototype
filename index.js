8504163ec2a30cc42870cca792218c288e6c621e
Brent Besselievre <brentbess21@gmail.com>
2021-07-21 13:03:57 -0600

    Solved task 1

--- index.js
+++ index.js
@@ -39,15 +39,31 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  } 
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
+const brent = new Person("Brent", 32);
 
-
+brent.eat("Pizza");
+brent.poop();
+console.log(brent);
 
 /*
   TASK 2
@@ -83,7 +99,7 @@ function Baby() {
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. This in the global scope returns the window 
   2. 
   3. 
   4. 
